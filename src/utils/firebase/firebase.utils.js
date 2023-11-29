3e9abec9ef78d2008178fa44d2ca29660ffcf0c5
Werokajo <werner@hintis.at>
2023-11-29 16:36:57 +0100

    97 sign-in

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,69 +1,57 @@
-import { initializeApp } from 'firebase/app';
-import { 
-    getAuth, 
-    signInWithRedirect,
-    signInWithPopup,
-    GoogleAuthProvider
-} from 'firebase/auth';
+import { initializeApp } from "firebase/app";
 import {
-    getFirestore,
-    doc,
-    getDoc,
-    setDoc
-} from 'firebase/firestore'
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDVyjbmm4LL1d9J_Yz7lqdnSB35A7_VJ-U",
-    authDomain: "crwn-clothing-db-2a101.firebaseapp.com",
-    projectId: "crwn-clothing-db-2a101",
-    storageBucket: "crwn-clothing-db-2a101.appspot.com",
-    messagingSenderId: "274591182429",
-    appId: "1:274591182429:web:571dbe943a8da66f9f27b4"
-  };
-  
-  // Initialize Firebase
-  const firebaseApp = initializeApp(firebaseConfig);
+  apiKey: "AIzaSyDVyjbmm4LL1d9J_Yz7lqdnSB35A7_VJ-U",
+  authDomain: "crwn-clothing-db-2a101.firebaseapp.com",
+  projectId: "crwn-clothing-db-2a101",
+  storageBucket: "crwn-clothing-db-2a101.appspot.com",
+  messagingSenderId: "274591182429",
+  appId: "1:274591182429:web:571dbe943a8da66f9f27b4",
+};
 
-  const provider = new GoogleAuthProvider();
+const firebaseApp = initializeApp(firebaseConfig);
 
-  provider.setCustomParameters({
-    prompt: "select_account"
-  });
+const googleProvider = new GoogleAuthProvider();
 
-  export const auth = getAuth();
-  export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+googleProvider.setCustomParameters({
+  prompt: "select_account",
+});
 
-  export const db = getFirestore();
+export const auth = getAuth();
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
 
-  export const createUserDocumentFromAuth = async (userAuth) => {
-    const userDocRef = doc(db, 'users', userAuth.uid);
+export const db = getFirestore();
 
-    console.log(userDocRef);
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
 
-    const userSnapshot = await getDoc(userDocRef);
-    console.log(userSnapshot);
-    console.log(userSnapshot.exists());
+  const userSnapshot = await getDoc(userDocRef);
 
-    if (!userSnapshot.exists()) {
-      const { displayName, email } = userAuth;
-      const createdAt = new Date();
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
 
-      try {
-        await setDoc(userDocRef, {
-          displayName,
-          email,
-          createdAt
-        });
-      } catch (error) {
-        console.log('Error creating the user', error.message);
-      }
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
     }
-
-    return userDocRef;
-
-    // if user data exists
-    // return userDocRef
-
-
   }
+
+  return userDocRef;
+};
